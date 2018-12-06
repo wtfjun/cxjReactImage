c8cb2bf688977abc3e965204ac72c361c1fa691f
chenxj <chenxjf@yonyou.com>
2018-12-06 17:18:41 +0800

    部署

---
+++ build/precache-manifest.5b13e81627b727abe036b868e66a0f2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e119ac4fafbf18551002",
+    "url": "/static/js/main.e119ac4f.chunk.js"
+  },
+  {
+    "revision": "2812b43ab0b779c0717a",
+    "url": "/static/js/1.2812b43a.chunk.js"
+  },
+  {
+    "revision": "e119ac4fafbf18551002",
+    "url": "/static/css/main.81877e8e.chunk.css"
+  },
+  {
+    "revision": "d41cb1899f844ebcff0e75f969ebe990",
+    "url": "/index.html"
+  }
+];
